1945b7d1e9e023b4fe6b2d229579c5502cdcdaa7
firefingers21 <online+github@alexhuisman.ca>
2026-08-20 22:11:40 -0700

    Optimize downloading of data

--- Workflow/scripts/reload.sh
+++ Workflow/scripts/reload.sh
@@ -6,11 +6,26 @@ seasonDir="${alfred_workflow_data}/${seasonYear}"
 
 # Get season standings
 mkdir -p "${seasonDir}"
-curl -sf --compressed --parallel --connect-timeout 10 \
+curl --parallel --max-time 10 \
+    -sf --compressed \
+    --etag-compare "${seasonDir}/standings.etag" \
+    --etag-save "${seasonDir}/standings.etag.tmp" \
     -L "https://standings.uefa.com/v1/standings?competitionId=1&seasonYear=${seasonYear}" -o "${seasonDir}/standings.json" \
+    --next \
+    -sf --compressed \
+    --etag-compare "${seasonDir}/stats1.etag" \
+    --etag-save "${seasonDir}/stats1.etag.tmp" \
     -L "https://compstats.uefa.com/v1/team-ranking?competitionId=1&limit=50&offset=0&seasonYear=${seasonYear}&stats=goals%2Cgoals_scored_with_right%2Cgoals_scored_with_left%2Cgoals_scored_head%2Cgoals_scored_other%2Cgoals_scored_inside_penalty_area%2Cgoals_scored_outside_penalty_area%2Cpenalty_scored%2Cattempts%2Cattempts_on_target%2Cattempts_off_target%2Cattempts_blocked%2Cpasses_accuracy%2Cpasses_attempted%2Cpasses_completed%2Cball_possession%2Ccross_accuracy%2Ccross_attempted%2Ccross_completed%2Cfree_kick%2Cattacks%2Cassists%2Ccorners%2Coffsides%2Cdribbling%2Crecovered_ball%2Ctackles%2Ctackles_won%2Ctackles_lost%2Cclearance_attempted" -o "${seasonDir}/stats1.json" \
+    --next \
+    -sf --compressed \
+    --etag-compare "${seasonDir}/stats2.etag" \
+    --etag-save "${seasonDir}/stats2.etag.tmp" \
     -L "https://compstats.uefa.com/v1/team-ranking?competitionId=1&limit=50&offset=0&seasonYear=${seasonYear}&stats=saves%2Cgoals_conceded%2Cown_goal_conceded%2Csaves_on_penalty%2Cclean_sheet%2Cpunches%2Cfouls_committed%2Cfouls_suffered%2Cyellow_cards%2Cred_cards" -o "${seasonDir}/stats2.json" \
 && downloadStatus=1
+# Update/Delete etag files
+for etag_tmp in ${seasonDir}/*.etag.tmp(N); do
+    [[ -s "${etag_tmp}" ]] && mv "${etag_tmp}" "${etag_tmp%.tmp}" || rm -f "${etag_tmp}"
+done
 
 if [[ -n "${downloadStatus}" ]]; then
     set -o extendedglob
@@ -18,7 +33,7 @@ if [[ -n "${downloadStatus}" ]]; then
         # Get Team Logos
         mkdir -p "${seasonDir}/icons"
         teamLogos=($(jq -r '.[].items[].team.mediumLogoUrl' "${seasonDir}/standings.json"))
-        curl -sf --compressed --parallel --output-dir "${seasonDir}/icons" --remote-name-all -L "${teamLogos[@]}"
+        curl -sf --compressed --parallel --max-time 10 --output-dir "${seasonDir}/icons" --remote-name-all -L "${teamLogos[@]}"
     fi
     touch "${alfred_workflow_data}"
     printf "Standings Updated"
